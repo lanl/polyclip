c7963c33fdbff0dd4fc1bc433f112afb4107d4c5
Melanie Walsmith <mcwalsmith@lanl.gov>
2024-07-17 16:22:26 -0600

    Generalize Interface

--- main.cpp
+++ main.cpp
@@ -8,18 +8,23 @@ int main(int argc, const char * argv[]){
     bool horizontal = false;
     bool four = false;
 
+
     // 4 by 4 Case //////////////////////////////
     if(four){
         int n_cells = 4;
         Mesh mesh(n_cells);
-        std::vector<std::array<Point, 2>> line(4);
+        int n_nodes = n_cells + 1; 
+        std::vector<std::array<Point, 2>> line(n_cells);
 
         if(horizontal){
-            // Interface //////////////////////// 
-            line[0] = {{{2.0, 0.10}, {-1.0, 0.10}}};    // ccw
-            line[1] = {{{2.0, 0.4}, {-1.0, 0.4}}};      // ccw
-            line[2] = {{{2.0, 0.6}, {-1.0, 0.6}}};      // ccw
-            line[3] = {{{2.0, 0.85}, {-1.0, 0.85}}};    // ccw 
+            // Interface ////////////////////////
+            for(int i = 0; i < line.size(); i++){
+                int y1 = i * n_nodes;
+                int y2 = y1 + n_nodes;
+                double val = (mesh.points_[y1].y + mesh.points_[y2].y) / 2;
+
+                line[i] = {{{2.0, val}, {-1.0, val}}};
+            }
 
             // Clipping below for Every Cell ////
             for(int c = 0; c < (n_cells * n_cells); c++){
@@ -30,10 +35,13 @@ int main(int argc, const char * argv[]){
         }
         else{
             // Interface ////////////////////////
-            line[0] = {{{.1, -1}, {.1, 2}}};    // ccw
-            line[1] = {{{.4, -1}, {.4, 2}}};    // ccw
-            line[2] = {{{.6, -1}, {.6, 2}}};    // ccw
-            line[3] = {{{.8, -1}, {.8, 2}}};    // ccw
+            for(int i = 0; i < line.size(); i++){
+                int x1 = i;
+                int x2 = x1 + 1;
+                double val = (mesh.points_[x1].x + mesh.points_[x2].x) / 2;
+
+                line[i] = {{{val, -1.0}, {val, 2.0}}};
+            }
 
             // Clipping below for Every Cell ////
             for(int k = 0; k < line.size(); k++){
@@ -49,27 +57,18 @@ int main(int argc, const char * argv[]){
     else{
         int n_cells = 16;
         Mesh mesh(n_cells);
+        int n_nodes = n_cells + 1; 
         std::vector<std::array<Point, 2>> line(n_cells);
 
         if(horizontal){
             // Interface ////////////////////////
-            line[0] = {{{2.0, 0.03}, {-1.0, 0.03}}};    // ccw
-            line[1] = {{{2.0, 0.09}, {-1.0, 0.09}}};    // ccw
-            line[2] = {{{2.0, 0.14}, {-1.0, 0.14}}};    // ccw
-            line[3] = {{{2.0, 0.2}, {-1.0, 0.2}}};      // ccw
-            line[4] = {{{2.0, 0.29}, {-1.0, 0.29}}};    // ccw
-            line[5] = {{{2.0, 0.35}, {-1.0, 0.35}}};    // ccw
-            line[6] = {{{2.0, 0.4}, {-1.0, 0.4}}};      // ccw
-            line[7] = {{{2.0, 0.45}, {-1.0, 0.45}}};    // ccw
+            for(int i = 0; i < line.size(); i++){
+                int y1 = i * n_nodes;
+                int y2 = y1 + n_nodes;
+                double val = (mesh.points_[y1].y + mesh.points_[y2].y) / 2;
 
-            line[8] = {{{2.0, 0.53}, {-1.0, 0.53}}};    // ccw
-            line[9] = {{{2.0, 0.6}, {-1.0, 0.6}}};      // ccw
-            line[10] = {{{2.0, 0.65}, {-1.0, 0.65}}};   // ccw
-            line[11] = {{{2.0, 0.72}, {-1.0, 0.72}}};   // ccw
-            line[12] = {{{2.0, 0.79}, {-1.0, 0.79}}};   // ccw
-            line[13] = {{{2.0, 0.85}, {-1.0, 0.85}}};   // ccw
-            line[14] = {{{2.0, 0.9}, {-1.0, 0.9}}};     // ccw
-            line[15] = {{{2.0, 0.95}, {-1.0, 0.95}}};   // ccw
+                line[i] = {{{2.0, val}, {-1.0, val}}};
+            }
 
             // Clipping below for Every Cell ////
             for(int c = 0; c < (n_cells * n_cells); c++){
@@ -81,24 +80,13 @@ int main(int argc, const char * argv[]){
         }
         else{
             // Interface ////////////////////////
-            line[0] = {{{0.03, -1}, {0.03, 2}}};      // ccw
-            line[1] = {{{0.09, -1}, {0.09, 2}}};      // ccw
-            line[2] = {{{0.14, -1}, {0.14, 2}}};      // ccw
-            line[3] = {{{0.2, -1}, {0.2, 2}}};        // ccw
-            line[4] = {{{0.29, -1}, {0.29, 2}}};      // ccw
-            line[5] = {{{0.35, -1}, {0.35, 2}}};      // ccw
-            line[6] = {{{0.4, -1}, {0.4, 2}}};        // ccw
-            line[7] = {{{0.45, -1}, {0.45, 2}}};      // ccw
+            for(int i = 0; i < line.size(); i++){
+                int x1 = i;
+                int x2 = x1 + 1;
+                double val = (mesh.points_[x1].x + mesh.points_[x2].x) / 2;
 
-            line[8] = {{{0.53, -1}, {0.53, 2}}};      // ccw
-            line[9] = {{{0.6, -1}, {0.6, 2}}};        // ccw
-            line[10] = {{{0.65, -1}, {0.65, 2}}};     // ccw
-            line[11] = {{{0.72, -1}, {0.72, 2}}};     // ccw
-            line[12] = {{{0.79, -1}, {0.79, 2}}};     // ccw
-            line[13] = {{{0.85, -1}, {0.85, 2}}};     // ccw
-            line[14] = {{{0.9, -1}, {0.9, 2}}};       // ccw
-            line[15] = {{{0.95, -1}, {0.95, 2}}};     // ccw
-
+                line[i] = {{{val, -1.0}, {val, 2.0}}};
+            }
 
             // Clipping below for Every Cell ////
             for(int k = 0; k < line.size(); k++){
