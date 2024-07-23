262c1a71e3f1ab188656638649841eeaf1514f10
Melanie Walsmith <mcwalsmith@lanl.gov>
2024-07-23 17:06:18 -0600

    Plotting Kokkos and OpenMP times

--- kokkos/MultipleThreads.sh
+++ kokkos/MultipleThreads.sh
@@ -1,14 +1,29 @@
 # Create file ########################################
-output_file="TimeResults.txt" > $output_file
+output_file="TimeResults_Kokkos.txt" > $output_file
 
 # Iterate Through Threads ############################
-for threads in 1 2 4 8       
+for threads in 1 2 4 8 
 do 
   # Collect 5 duration times for every thread ########
   for i in {1 2 3 4 5}         
   do 
   export OMP_NUM_THREADS=$threads
   duration=$(./clip_kokkos | grep "Duration" | awk '{print $2}')
+  echo "Threads: $threads, Duration: $duration" >> $output_file
+  done
+done  
+
+# Create file ########################################
+output_file="TimeResults_OpenMP.txt" > $output_file
+
+# Iterate Through Threads ############################
+for threads in 1 2 4 8   
+do 
+  # Collect 5 duration times for every thread ########
+  for i in {1 2 3 4 5}         
+  do 
+  export OMP_NUM_THREADS=$threads
+  duration=$(./allclips | grep "Duration" | awk '{print $2}')
   echo "Threads: $threads, Duration: $duration" >> $output_file
   done
 done  
--- kokkos/PlotTimeofThreads.py
+++ kokkos/PlotTimeofThreads.py
@@ -1,39 +1,52 @@
 #!/usr/bin/python3
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 import numpy as np
 
-time_data = {}
+def compute_avg_time(filename):
+    time_data = {}
 
-# Open and read file containing timing data ##########
-with open("TimeResults.txt", "r") as file:
-    for line in file:
-        if "Threads" in line and "Duration" in line:
-            parts = line.strip().split(", ")
-            threads = int(parts[0].split(": ")[1]) 
-            time = int(parts[1].split(": ")[1]) 
+    # Open and read file containing timing data ##########
+    with open(filename, "r") as file:
+        for line in file:
+            if "Threads" in line and "Duration" in line:
+                parts = line.strip().split(", ")
+                threads = int(parts[0].split(": ")[1]) 
+                time = int(parts[1].split(": ")[1]) 
 
-            if threads not in time_data:
-                time_data[threads] = []
+                if threads not in time_data:
+                    time_data[threads] = []
 
-        time_data[threads].append(time)     # Store the timings
+            time_data[threads].append(time)     # Store the timings
 
-# Obtain the Threads ################################# 
-threads = sorted(time_data.keys())
+    # Obtain the Threads ################################# 
+    threads = sorted(time_data.keys())
 
-# Calculate the Average ##############################
-avg = []
-for t in threads:
-    if time_data[t]:
-        avg.append(np.mean(time_data[t]))
-    else:
-        avg.append(float('nan'))
+    # Calculate the Average ##############################
+    avg = []
+    for t in threads:
+        if time_data[t]:
+            avg.append(np.mean(time_data[t]))
+        else:
+            avg.append(float('nan'))
+    return (threads, avg)            
 
+if __name__ == '__main__':
+    
+    (threads_openmp, avg_openmp) = compute_avg_time("TimeResults_OpenMP.txt")
+    (threads_kokkos, avg_kokkos) = compute_avg_time("TimeResults_Kokkos.txt")
 
-# Plot the Averages of the Threads ##################
-plt.plot(threads, avg, marker='o', linestyle='-', color='pink', markerfacecolor='blue', linewidth = 3)
-plt.xlabel('Threads')
-plt.ylabel('Time (ms)')
-plt.title('Kokkos Clipping Duration over Threads')
-plt.grid(True)
-plt.xticks(threads)
-plt.show()
+    # Plot the Averages of the Threads ##################
+    plt.plot(threads_openmp, avg_kokkos, marker='o', linestyle='-', color='pink', markerfacecolor='blue', linewidth = 3, label='kokkos')
+    plt.plot(threads_openmp, avg_openmp, marker='o', linestyle='-', color='green', markerfacecolor='blue', linewidth = 3, label='openmp')
+    plt.xlabel('Threads')
+    plt.ylabel('Time (ms)')
+    plt.title('Clipping Strong Scaling, mesh size: 120x120 cells')
+    plt.grid(True)
+    plt.xticks(threads_openmp)
+    plt.yscale('log', base=2)
+    plt.xscale('log', base=2)
+    ax = plt.figure().gca()
+    plt.xtics.
+    plt.legend()
+    plt.show()
