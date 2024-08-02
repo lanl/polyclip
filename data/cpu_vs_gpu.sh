1da8f7e76a16e670137497469601b15ecb1e1ca4
mcwalsmith <mcwalsmith@lanl.gov>
2024-08-02 12:00:40 -0600

    Gpu Raw Data

---
+++ data/cpu_vs_gpu.sh
@@ -0,0 +1,61 @@
+# Arguments for 120x120 and 80x80 ####################
+arg_1_120="120" 
+arg_2_120="30"
+arg_1_80="80"
+arg_2_80="20"
+
+# Kokkos CPU file ####################################
+output_file="cpu_120.txt" > $output_file   # 120x120
+
+# Iterate Through Threads 
+# Collect 5 duration times for every thread 
+for i in {1 2 3 4 5}         
+do 
+export OMP_NUM_THREADS=16
+threads=16
+duration=$(./clip_kokkos "$arg_1_120" "$arg_2_120" | grep "Duration" | awk '{print $2}')
+threads=$(./clip_kokkos "$arg_1_120" "$arg_2_120" | grep "Max Threads" | awk '{print $3}')
+echo "Threads: $threads, Duration: $duration" >> $output_file
+done
+
+# Kokkos CPU file ####################################
+output_file="cpu_80.txt" > $output_file   # 120x120
+
+# Iterate Through Threads 
+# Collect 5 duration times for every thread 
+for i in {1 2 3 4 5}         
+do 
+export OMP_NUM_THREADS=16
+threads=16
+duration=$(./clip_kokkos "$arg_1_80" "$arg_2_80" | grep "Duration" | awk '{print $2}')
+threads=$(./clip_kokkos "$arg_1_120" "$arg_2_120" | grep "Max Threads" | awk '{print $3}')
+echo "Threads: $threads, Duration: $duration" >> $output_file
+done
+  
+
+## Kokkos GPU file ####################################
+#output_file="gpu_120.txt" > $output_file   # 120x120
+#
+## Iterate Through Threads 
+## Collect 5 duration times for every thread 
+#for i in {1 2 3 4 5}         
+#do 
+#duration=$(./clip_gpu "$arg_1_120" "$arg_2_120" | grep "Duration" | awk '{print $2}')
+#threads=$(./clip_gpu "$arg_1_120" "$arg_2_120" | grep "Max Threads" | awk '{print $3}')
+#echo "Threads: $threads, Duration: $duration" >> $output_file
+#done
+#
+## Kokkos GPU file ####################################
+#output_file="gpu_80.txt" > $output_file   # 80x80
+#
+## Iterate Through Threads 
+## Collect 5 duration times for every thread 
+#for i in {1 2 3 4 5}         
+#do 
+#duration=$(./clip_gpu "$arg_1_80" "$arg_2_80" | grep "Duration" | awk '{print $2}')
+#threads=$(./clip_gpu "$arg_1_120" "$arg_2_120" | grep "Max Threads" | awk '{print $3}')
+#echo "Threads: $threads, Duration: $duration" >> $output_file
+#done
+
+
+
