20d6d4905df1a13927af33571a25369b8dc9539d
mcwalsmith <mcwalsmith@lanl.gov>
2025-01-09 13:24:57 -0700

    Clean up code.

--- test/mesh_gpu.h
+++ test/mesh_gpu.h
@@ -28,33 +28,3 @@ namespace polyintersect {
   };
 }
 
-
-/*#pragma once      //prevent headers being used twice
-
-#include <Kokkos_Core.hpp>
-#include <Kokkos_Vector.hpp>
-#include "geometry_gpu.h"
-
-namespace polyintersect {
-
-// Mesh containing the coordinates and the cell
-  class Mesh_Kokkos {
-  public:
-    // constructor
-    Mesh_Kokkos(int total_points, int total_cells, int max_edges_per_cell);
-
-    // destructor
-    ~Mesh_Kokkos() = default;
-
-    // create list of all the points and create cells
-    void add_all_points(int point_index, Point coordinates, Kokkos::View<Point*> points_);
-    void add_edge(int cell_index, int edge, Edge const& points, Kokkos::View<int***> cells_);
-
-    // mesh data members
-    Kokkos::View<Point*, Kokkos::CudaSpace> points_;
-    Kokkos::View<int***, Kokkos::CudaSpace> cells_;
-    
-    Kokkos::View<Point*>::HostMirror mirror_points_;
-    Kokkos::View<int***,>::HostMirror mirror_cells_;
-  };
-}*/
