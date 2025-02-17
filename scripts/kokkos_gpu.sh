96566eefb9f170f65eaa33a52419e90ce06d6de3
Jonathan Mathurin <jmathurin@lanl.gov>
2025-02-16 22:36:38 -0700

    Forgot to do git add for my other changes. Commiting it now.

--- scripts/kokkos_gpu.sh
+++ scripts/kokkos_gpu.sh
@@ -2,8 +2,15 @@ if [ -f "CMakeCache.txt" ]; then
   rm CMakeCache.txt
 fi
 
+
+if [ ! -f "kokkos_config.txt" ]; then
+  echo "kokkos_config.txt file not found! Run set_kokkos_root.sh [KOKKOS_INSTALL_PATH] first."
+  exit 1
+fi
+
+KOKKOS_ROOT=$(cat "kokkos_config.txt")
 cmake \
-  -DKokkos_ROOT="/vast/home/jmathurin/kokkos-gpu/install" \
+  -DKokkos_ROOT="$KOKKOS_ROOT" \
   -DUSE_KOKKOS_GPU=ON \
   ..
 
