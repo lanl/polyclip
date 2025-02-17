7841d470c7f88f91c40fd727bd26892bcffb7d97
Jonathan Mathurin <jmathurin@lanl.gov>
2025-02-17 12:17:17 -0700

    Ok, so I have a pretty good understanding of the basic format for the gmv writer. Going to organize what I learned into a gmvstream namespace.

---
+++ gmv_tool/gmv_writer.cpp
@@ -0,0 +1,44 @@
+//
+// Created by Jonathan Mathurin on 2/16/25.
+//
+
+#include "gmv_writer.h"
+#include <iostream>
+#include <fstream>
+int main(int argc, char* argv[]){
+
+  std::ofstream clear_file("test.gmv", std::ios::trunc);
+  std::ofstream test_file("test.gmv", std::ios::app);
+
+  test_file << "gmvinput ascii\n";
+  test_file << "nodev 11\n";
+  test_file << std::scientific << std::setprecision(17);
+  test_file << 0.0  << " " << 0.0 << " " << 0.0 << "\n";
+  test_file << 0.5  << " " << 0.0 << " " << 0.0 << "\n";
+  test_file << 0.5  << " " << 0.25 << " " << 0.0 << "\n";
+  test_file << 0.25  << " " << 0.375 << " " << 0.0 << "\n";
+  test_file << 0.0  << " " << 0.25 << " " << 0.0 << "\n";
+  test_file << 0.875  << " " << 0.25 << " " << 0.0 << "\n";
+  test_file << 1.0  << " " << 0.375 << " " << 0.0 << "\n";
+  test_file << 0.875  << " " << 0.625 << " " << 0.0 << "\n";
+  test_file << 0.5  << " " << 0.875 << " " << 0.0 << "\n";
+  test_file << 0.5  << " " << 0.875 << " " << 0.0 << "\n";
+  test_file << 0.875  << " " << 0.875 << " " << 0.0 << "\n";
+  test_file << "cells " << 4 << "\n";
+  test_file << "general 1 5 1 2 3 4 5\n";
+  test_file << "general 1 3 2 6 3\n";
+  test_file << "general 1 6 3 6 7 8 9 4\n";
+  test_file << "general 1 4 9 8 11 10\n";
+
+  // test_file << "general 2";
+  // test_file << "general 3";
+  // test_file << "general 4";
+  // test_file << "general 5";
+  test_file << "endgmv\n";
+
+  test_file.close();
+  std::cout << "I like eggs. . " << std::endl;
+
+
+  return 0;
+}
---
+++ gmv_tool/gmv_writer.h
@@ -0,0 +1,16 @@
+//
+// Created by Jonathan Mathurin on 2/16/25.
+//
+
+#ifndef GMV_READER_H
+#define GMV_READER_H
+
+
+
+class gmv_writer {
+
+};
+
+
+
+#endif //GMV_READER_H
