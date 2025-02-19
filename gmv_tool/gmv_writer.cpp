6d292c24bc769388e9a01c90230e93b2ce183f6c
Jonathan Mathurin <jmathurin@lanl.gov>
2025-02-19 14:28:58 -0700

    FOrgot, gmv start from 1, not 0.

--- gmv_tool/gmv_writer.cpp
+++ gmv_tool/gmv_writer.cpp
@@ -54,12 +54,12 @@ void gmvstream::write_gmv(polyintersect::Mesh_Kokkos mesh, const std::string& fi
       auto& node_val = mesh.mirror_cells_(i, j, 0);
       if(node_val != -1) {
         number_of_vertex++;
-        store_points += std::to_string(node_val) + " ";
+        store_points += std::to_string(node_val + 1) + " ";
         //gmv_file << edge_node.a << " ";
       }
     }
 
-    auto& node_val = mesh.mirror_cells_(i,number_of_iterations , 0);
+    auto& node_val = mesh.mirror_cells_(i,number_of_iterations-1, 0);
     if(node_val != -1) {
       store_points += std::to_string(node_val);
       number_of_vertex++;
