8c2bd7590104c186b2159a6b3da40b6f6bf6d6ac
Jonathan Mathurin <jmathurin@lanl.gov>
2025-04-14 21:54:26 -0600

    Forgot to make my comparison have exit failure.

--- test/compare_outputs.cpp
+++ test/compare_outputs.cpp
@@ -5,9 +5,12 @@ int main(int argc, char* argv[]) {
   if(argc < 3) {
     std::cout << "USAGE: compare_outputs [FILE_1] [FILE_2]\n";
   }
-  if(polyclip::io::compare_files(argv[1], argv[2])) {
-    std::cout << "Perfect match between files " << argv[1] << " and " << argv[2] << "\n";
+  if(!polyclip::io::compare_files(argv[1], argv[2])) {
+    std::cout << argv[1] << " and " << argv[2] << " are not equivalent!\n";
+    std::exit(EXIT_FAILURE);
    }
+
+  std::cout << "Perfect match between files " << argv[1] << " and " << argv[2] << "\n";
 
   return 0;
 }
