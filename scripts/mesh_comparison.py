36e166c93787ede6e9933365b88558c065d82408
mcwalsmith <mcwalsmith@lanl.gov>
2025-07-29 20:10:55 -0600

    Graph comparing all kokkos regions in 0.08 unit mesh

--- scripts/mesh_comparison.py
+++ scripts/mesh_comparison.py
@@ -2,8 +2,12 @@ import os
 import matplotlib.pyplot as plt
 
 # Data from all meshes
-timed_values = [4.9006, 5.6175, 5.6354, 6.9660]
-total_cells = [1, 4, 8, 16]
+timed_values_clipped_transfer_1 = [0.0600, 0.0601, 0.0588, 0.0603, 0.0592, 0.0600, 0.0603, 0.0597]
+timed_values_mesh_transfer = [0.0840, 0.0852, 0.0852, 0.0851, 0.0887, 0.0829, 0.0845, 0.0847]
+timed_values_clipping = [0.3358, 0.3999, 0.4375, 0.4636, 0.5079, 0.5406, 0.5645, 0.6108]
+timed_values_clipped_transfer_2 = [0.2418, 0.3206, 0.3781, 0.4453, 0.4997, 0.5522, 0.6065, 0.6585]
+
+total_cells = [28, 100, 139, 180, 226, 260, 289, 318]
 
 # Create new directory
 out_dir = "compare"
@@ -11,11 +15,17 @@ os.makedirs(out_dir, exist_ok=True)
 
 # Create plot
 plt.figure(figsize=(8, 5))
-plt.plot(total_cells, timed_values, marker='o', linestyle='-', color='blue')
+plt.plot(total_cells, timed_values_clipped_transfer_1, marker='o', linestyle='-', label='CLIPPED PART: CPU-TO-GPU TRANSFER', color='blue')
+plt.plot(total_cells, timed_values_mesh_transfer, marker='s', linestyle='-', label='MESH: CPU-TO-GPU TRANSFER', color='orange')
+plt.plot(total_cells, timed_values_clipping, marker='^', linestyle='-', label='CLIPPING CELLS', color='green')
+plt.plot(total_cells, timed_values_clipped_transfer_2, marker='d', linestyle='-', label='CLIPPED PART: GPU-TO-CPU TRANSFER', color='red')
+
 plt.ylabel("Time (ms)")
-plt.xlabel("Number of Lines Clipping")
-plt.title("Duration of Clipping Below Cells Region Across Multiple Lines \nin a 0.08 Unit Mesh")
+plt.xlabel("Number of Cells Clipped")
+plt.title("Runtime Analysis of all Kokkos Regions\nin a 0.08 Unit Mesh")
 plt.grid(True)
+
+plt.legend(loc='upper left', title='Kokkos Regions')
 
 # Save as figure
 out_path = os.path.join(out_dir, "mesh_compare_time_plot.png")
